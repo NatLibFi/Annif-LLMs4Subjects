239ab1ae50287d2bbdc4c379ab9162b0b74b2809
Inkinen Juho M M <34240031+juhoinkinen@users.noreply.github.com>
2025-01-23 16:34:00 +0200

    Separate slurm config for stages needing only CPU and CPU+GPU

--- tools/dvc-train-eval/run-all-dvc-train-eval.sh
+++ tools/dvc-train-eval/run-all-dvc-train-eval.sh
@@ -2,7 +2,12 @@
 
 for vocab in all tib-core; do
   for lang in de en; do
-    sbatch dvc-train-eval.sbatch $vocab $lang
+    # CPU stages
+    sbatch -p short -c 32 dvc-train-eval.sbatch $vocab $lang eval@$vocab-mllm-$lang
+    sbatch -p short -c 32 dvc-train-eval.sbatch $vocab $lang eval@$vocab-bonsai-$lang
+    # GPU stages
+    sbatch -p gpu-oversub -c 1 dvc-train-eval.sbatch $vocab $lang eval@$vocab-xtransformer-$lang
+    sbatch -p gpu-oversub -c 1 dvc-train-eval.sbatch $vocab $lang eval@$vocab-ensemble-$lang
   done
 done
 
