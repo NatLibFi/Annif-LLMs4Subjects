9d952b0e0233ec17693268f647589a649f195217
Inkinen Juho M M <34240031+juhoinkinen@users.noreply.github.com>
2025-01-27 15:05:56 +0200

    Train Xtransformer on orig. data only; eval with 1 job only; disable ensemble eval

--- tools/dvc-train-eval/run-all-dvc-train-eval.sh
+++ tools/dvc-train-eval/run-all-dvc-train-eval.sh
@@ -3,11 +3,11 @@
 for vocab in all tib-core; do
   for lang in de en; do
     # CPU stages
-    sbatch -p short -c 32 dvc-train-eval.sbatch $vocab mllm $lang
-    sbatch -p short -c 32 dvc-train-eval.sbatch $vocab bonsai $lang
+    ANNIF_EVAL_JOBS=32 sbatch -p short -c 32 dvc-train-eval.sbatch $vocab mllm $lang
+    ANNIF_EVAL_JOBS=32 sbatch -p short -c 32 dvc-train-eval.sbatch $vocab bonsai $lang
     # GPU stages
-    sbatch -p gpu -c 32 -t8:00:00 dvc-train-eval.sbatch $vocab xtransformer $lang
-    # sbatch -p gpu -c 32 dvc-train-eval.sbatch $vocab ensemble $lang
+    ANNIF_EVAL_JOBS=1 sbatch -p gpu-oversub -c 8 -t4:00:00 dvc-train-eval.sbatch $vocab xtransformer $lang
+    #ANNIF_EVAL_JOBS=1 sbatch -p gpu -c 32 dvc-train-eval.sbatch $vocab ensemble $lang
   done
 done
 
