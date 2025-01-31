96ed135e7c93ab1cb3ab9187c34994af8d16b8eb
Osma Suominen <osma.suominen@helsinki.fi>
2025-01-31 14:37:34 +0200

    eval results for bmx-ensemble

--- tools/dvc-train-eval/run-all-dvc-train-eval.sh
+++ tools/dvc-train-eval/run-all-dvc-train-eval.sh
@@ -8,8 +8,8 @@ for vocab in all tib-core; do
     #ANNIF_EVAL_JOBS=32 sbatch -p short -c 32 dvc-train-eval.sbatch $vocab bm-ensemble $lang
     #ANNIF_EVAL_JOBS=32 sbatch -p short -c 32 dvc-train-eval.sbatch $vocab bmx-ensemble $lang
     # GPU stages
-    ANNIF_EVAL_JOBS=1 sbatch -p gpu -G 1 -c 32 -t4:00:00 dvc-train-eval.sbatch $vocab xtransformer $lang
-    # ANNIF_EVAL_JOBS=1 sbatch -p gpu -G 1 -c 8 -t4:00:00 dvc-train-eval.sbatch $vocab ensemble $lang
+    #ANNIF_EVAL_JOBS=1 sbatch -p gpu -G 1 -c 32 -t4:00:00 dvc-train-eval.sbatch $vocab xtransformer $lang
+    ANNIF_EVAL_JOBS=1 sbatch -p gpu -G 1 -c 8 -t4:00:00 dvc-train-eval.sbatch $vocab bmx-ensemble $lang
   done
 done
 
