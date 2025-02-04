ebad809bb0aa3f4453e837983614ed3e5e894c66
Osma Suominen <osma.suominen@helsinki.fi>
2025-02-04 09:47:54 +0200

    add final submission files & adjusted scripts

--- tools/generate-predictions/all-predictions.sh
+++ tools/generate-predictions/all-predictions.sh
@@ -5,9 +5,17 @@ backend=$2 # e.g. bm-ensemble
 
 for vocab in all tib-core; do
   for lang in de en de,en; do
+    for origlang in de en; do
 
-    sbatch -p short -c 2 generate-predictions.sbatch gnd-$vocab-$backend-LANG $lang \
-      $subset-$vocab-$backend-$lang.zip \
-      ../../shared-task-datasets/TIBKAT/$vocab-subjects/data/$subset-*.jsonl.gz
+# for CPU jobs
+      echo sbatch -p medium -c 2 generate-predictions.sbatch gnd-$vocab-$backend-LANG $lang \
+        ../../submission/$subset-$vocab-$backend-$lang-$origlang.zip \
+        ../../shared-task-datasets/TIBKAT/$vocab-subjects/data/$subset-$origlang.jsonl.gz
+
+# for GPU jobs
+      echo sbatch -p gpu -G 1 -c 2 generate-predictions.sbatch gnd-$vocab-$backend-LANG $lang \
+        ../../submission/$subset-$vocab-$backend-$lang-$origlang.zip \
+        ../../shared-task-datasets/TIBKAT/$vocab-subjects/data/$subset-$origlang.jsonl.gz
+    done
   done
 done
